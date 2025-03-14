62fb35dd2cded135f09836bcf90db6388e27f06b
WhiteChameleon <115614289+WhiteChameleon@users.noreply.github.com>
2025-03-14 06:31:15 +0300

    test2

--- Script/Button.cs
+++ Script/Button.cs
@@ -5,7 +5,6 @@ public class Button : MonoBehaviour
 {
     public void StartGame()
     {
-        print("test");
         AppearAnimal.cowsCount[0] += 2;
         AppearAnimal.bullsCount[0] += 1;
         SceneManager.LoadScene("Game");
